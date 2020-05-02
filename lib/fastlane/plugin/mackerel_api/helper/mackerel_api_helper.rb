0fb9e7b2ad78fc8ca9140df4e23a164ea03e83f3
yutailang0119 <muta.yutaro@gmail.com>
2020-05-02 21:17:16 +0900

    Fix mackerel_api_heler to add self to methods

--- lib/fastlane/plugin/mackerel_api/helper/mackerel_api_helper.rb
+++ lib/fastlane/plugin/mackerel_api/helper/mackerel_api_helper.rb
@@ -1,20 +1,20 @@
 module Fastlane
   module Helper
     class MackerelApiHelper
-      def construct_headers(api_key, overrides)
+      def self.construct_headers(api_key, overrides)
         require 'base64'
         headers = { 'User-Agent' => 'fastlane-mackerel_api' }
         headers['X-Api-Key'] = Base64.strict_encode64(api_key).to_s if api_key
         headers.merge(overrides || {})
       end
 
-      def construct_url(server_url, path, url)
+      def self.construct_url(server_url, path, url)
         return_url = (server_url && path) ? File.join(server_url, path) : url
         UI.user_error!("Please provide either `server_url` (e.g. https://api.mackerelio.com) and 'path' or full 'url' for Mackerel API endpoint") unless return_url
         return_url
       end
 
-      def construct_body(body, raw_body)
+      def self.construct_body(body, raw_body)
         body ||= {}
         if raw_body
           raw_body
@@ -28,13 +28,13 @@ module Fastlane
         end
       end
 
-      def parse_json(value)
+      def self.parse_json(value)
         JSON.parse(value)
       rescue JSON::ParserError
         nil
       end
 
-      def call_endpoint(url, http_method, headers, body, secure)
+      def self.call_endpoint(url, http_method, headers, body, secure)
         require 'excon'
         Excon.defaults[:ssl_verify_peer] = secure
         middlewares = Excon.defaults[:middlewares] + [Excon::Middleware::RedirectFollower] # allow redirect in case of repo renames
