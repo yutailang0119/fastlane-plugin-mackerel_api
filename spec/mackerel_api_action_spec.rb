921cadddf267dd73942ba45af91751a812ff3ae8
yutailang0119 <muta.yutaro@gmail.com>
2020-05-03 21:49:23 +0900

    Fix mackrel_api_action spec

--- spec/mackerel_api_action_spec.rb
+++ spec/mackerel_api_action_spec.rb
@@ -1,9 +1,11 @@
 describe Fastlane::Actions::MackerelApiAction do
   describe '#run' do
     it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The mackerel_api plugin is working!")
+      # expect(Fastlane::UI).to receive(:message).with("The mackerel_api plugin is working!")
 
-      Fastlane::Actions::MackerelApiAction.run(nil)
+      Fastlane::Actions::MackerelApiAction.run(server_url: 'https://api.mackerelio.com',
+                                               path: 'api/v0/services',
+                                               api_key: ENV['FL_MACKEREL_API_KEY'])
     end
   end
 end
