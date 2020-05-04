0e45b26950092f4b0d74d3372955ffaac7d6ba91
yutailang0119 <muta.yutaro@gmail.com>
2020-05-05 01:44:47 +0900

    Fix spec to add test for construct headers

--- spec/mackerel_api_action_spec.rb
+++ spec/mackerel_api_action_spec.rb
@@ -9,4 +9,35 @@ describe Fastlane::Actions::MackerelApiAction do
       expect(result[:json].key?("name")).to be(true)
     end
   end
+
+  describe '#MackerelApiHelper.construct_headers' do
+    it 'construct headers' do
+      api_key = random_identifier(30)
+      headers = Fastlane::Helper::MackerelApiHelper.construct_headers(api_key, nil)
+
+      expect(headers).to eq({
+        'User-Agent' => 'fastlane-mackerel_api',
+        'X-Api-Key' => api_key
+      })
+    end
+
+    it 'construct headers with overrides' do
+      api_key = random_identifier(30)
+      overrides = { 'Foo' => 'bar' }
+      headers = Fastlane::Helper::MackerelApiHelper.construct_headers(api_key, overrides)
+
+      expect(headers).to eq({
+        'User-Agent' => 'fastlane-mackerel_api',
+        'X-Api-Key' => api_key,
+        'Foo' => 'bar'
+      })
+    end
+
+    private
+
+    def random_identifier(number)
+      charset = Array('A'..'Z') + Array('a'..'z')
+      Array.new(number) { charset.sample }.join
+    end
+  end
 end
