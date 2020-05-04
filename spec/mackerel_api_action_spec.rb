b6911741dbc508c58e63795aa18767d2f8ab9765
yutailang0119 <muta.yutaro@gmail.com>
2020-05-05 01:21:58 +0900

    Fix spec to add test to call Mackerel API

--- spec/mackerel_api_action_spec.rb
+++ spec/mackerel_api_action_spec.rb
@@ -1,11 +1,12 @@
 describe Fastlane::Actions::MackerelApiAction do
   describe '#run' do
-    it 'prints a message' do
-      # expect(Fastlane::UI).to receive(:message).with("The mackerel_api plugin is working!")
-
-      Fastlane::Actions::MackerelApiAction.run(server_url: 'https://api.mackerelio.com',
-                                               path: 'api/v0/services',
-                                               api_key: ENV['FL_MACKEREL_API_KEY'])
+    it 'call Mackerel API' do
+      result = Fastlane::Actions::MackerelApiAction.run(server_url: 'https://api.mackerelio.com',
+                                                        path: 'api/v0/org',
+                                                        api_key: ENV['FL_MACKEREL_API_KEY'])
+      expect(result[:status]).to eq(200)
+      expect(result[:body]["name"].nil?).to be(false)
+      expect(result[:json].key?("name")).to be(true)
     end
   end
 end
